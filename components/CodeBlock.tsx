9e62db1fc9744f4f23a7cbf93042767f47ffbdf1
pecet <pecet3107@gmail.com>
2024-04-29 23:43:03 +0200

    its done!

--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -1,59 +1,57 @@
+"use client"
 import { PL } from '@/content/content';
 import Typewriter from 'typewriter-effect';
 
 export const CodeBlock = () => {
     return (
-        // <div className="font-mono text-base sm:text-xl w-72 sm:w-96 pl-10
-        //  bg-slate-600 p-4 rounded-lg  border-t-[16px] border-4 border-slate-400">
-        //     <ul className='list-decimal'>
-        //         <li className="text-black">
-        //             <p className='text-fuchsia-500'>&lt;body&gt;</p>
-        //         </li>
-        //         <li className="text-black">
-        //             <p className='text-lime-300'>
-        //                 <Typewriter
-        //                     options={{
-        //                         strings: `Hej!, mam na imię Jakub`,
-        //                         autoStart: true,
-        //                         delay: 30,
-
-        //                     }}
-        //                 />
-        //             </p>
-        //         </li>
-        //         <li className="text-black">
-        //             <p className='text-fuchsia-500'>&lt;/body&gt;</p>
-        //         </li>
-        //     </ul>
-
-
-        // </div>
         <div className="font-mono text-base sm:text-xl w-72 sm:w-96 
-        bg-slate-600 p-4 rounded-lg  border-t-[16px] border-4 border-slate-400 
-        grid grid-cols-[24px_minmax(300px,_1fr)_100px] gap-0">
-
-            <div className="flex flex-col w-4">
-                <p className='text-black'>
+        bg-slate-700 px-2 py-4 rounded-lg  border-t-[20px] border-4 border-slate-200 
+        shadow-lg shadow-black
+        grid grid-cols-[32px_minmax(400px,_1fr)_100px] gap-0 relative">
+            <div className="absolute flex gap-1 -top-4">
+                <div className="w-3 h-3 bg-red-500 rounded-full"></div>
+                <div className="w-3 h-3 bg-yellow-500 rounded-full"></div>
+                <div className="w-3 h-3 bg-green-500 rounded-full"></div>
+            </div>
+            <div className="flex flex-col w-4 text-sky-400">
+                <p>
                     1.
                 </p>
-                <p className='text-black'>
+                <p>
                     2.
                 </p>
-                <p className='text-black'>
+                <p>
                     3.
                 </p>
-            </div>
-            <Typewriter
-                options={{
-                    strings: `
-                    <p class="text-fuchsia-400"> Hej!, mam na imię Jakub </p>
-                    
-                    `,
-                    autoStart: true,
-                    delay: 30,
+                <p>
+                    4.
+                </p>
+                <p>
+                    5.
+                </p>
+                <p>
+                    6.
+                </p>
 
-                }}
-            />
+            </div>
+            <div>
+                <p className="text-fuchsia-400">&lt;div&gt; </p>
+                <div className="text-lime-300 pl-4">
+                    <Typewriter
+                        options={{
+                            strings: `
+                     Hej!, mam na imię Jakub <br>
+                    chętnie wykonam dla Ciebie <br>
+                    piękną stronę internetową!`
+                            ,
+                            autoStart: true,
+                            delay: 40,
+
+                        }}
+                    />
+                </div>
+                <p className="text-fuchsia-400">&lt;/div&gt; </p>
+            </div>
         </div>
     )
 }
