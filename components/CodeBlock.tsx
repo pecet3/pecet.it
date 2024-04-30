34cb92aec27156181c40b2b11994d1b8a93e965d
pecet <pecet3107@gmail.com>
2024-04-30 20:42:10 +0200

    .

--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -3,13 +3,13 @@ import { PL } from '@/content/content';
 import Typewriter from 'typewriter-effect';
 
 export const CodeBlock = () => {
-    const rangeArray = Array.from({ length: 16 }, (_, index) => index + 1);
+    const rangeArray = Array.from({ length: 15 }, (_, index) => index + 1);
 
     return (
         <div className="font-mono text-sm sm:text-base  w-80 sm:w-[28rem] 
         bg-slate-700 px-2 py-2 rounded-lg  border-t-[20px] border-4 border-slate-200 
         shadow-lg shadow-black
-        grid grid-cols-[32px_minmax(300px,_1fr)] gap-0 relative">
+        grid grid-cols-[32px_minmax(0px,_1fr)] gap-0 relative">
             <div className="absolute flex gap-1 -top-4">
                 <div className="w-3 h-3 bg-red-500 rounded-full"></div>
                 <div className="w-3 h-3 bg-yellow-500 rounded-full"></div>
@@ -17,12 +17,18 @@ export const CodeBlock = () => {
             </div>
             <div className="flex flex-col w-4 text-sky-400">
                 {rangeArray.map((v) => (
-                    <p>{v}.</p>
+                    <p key={v}>{v}.</p>
                 ))}
             </div>
             <div>
                 <HTMLelement element='html' lang='pl'>
                     <HTMLelement element='head' >
+                        <p className="text-fuchsia-400">
+                            &lt;meta
+                            <b className="text-teal-400"> charset
+                                <b className="text-sky-400">=</b>
+                                <b className='text-red-400'>"utf-8"</b></b>&gt;
+                        </p>
                         <HTMLelement element='title' >
                             Twoja wymarzona strona
                         </HTMLelement>
@@ -58,7 +64,7 @@ interface HTMLelementProps {
     element: string;
     classes?: string;
     lang?: string;
-    children: React.ReactNode
+    children?: React.ReactNode
 }
 
 const HTMLelement: React.FC<HTMLelementProps> = ({ element, classes, lang, children }) => {
