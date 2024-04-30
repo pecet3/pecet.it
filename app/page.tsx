961faa1912edce43b05ab009d9e1a184f02bc73c
pecet <pecet3107@gmail.com>
2024-04-30 20:34:59 +0200

    working on code block

--- app/page.tsx
+++ app/page.tsx
@@ -11,24 +11,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center gap-12 justify-center p-24">
       <CodeBlock />
-      {/* <motion.div
-        initial={{ opacity: 0, scale: 0 }}
-        animate={{ opacity: 100, scale: 1 }}
-        transition={{ opacity: { delay: 2, duration: 1 }, scale: { delay: 1.5, duration: 1 } }}
 
-        className="font-mono border-2 group
-         border-black rounded-full py-2 px-4 hover:bg-slate-600 duration-300
-         hover:cursor-pointer shadow-xl shadow-slate-300 bg-zinc-100 hover:shadow-none
-         "
-      >
-
-        <Link href="/home" className="text-black group-hover:text-slate-300 font-mono font-bold text-4xl tracking-tighter flex duration-300 ">
-          &lt;<div className="group-hover:text-white text-slate-700">pecet</div>
-          .<span className="text-blue-800 group-hover:text-sky-200 ">it</span>/&gt;
-        </Link>
-
-
-      </motion.div> */}
     </main>
   );
 }
--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -3,55 +3,77 @@ import { PL } from '@/content/content';
 import Typewriter from 'typewriter-effect';
 
 export const CodeBlock = () => {
+    const rangeArray = Array.from({ length: 16 }, (_, index) => index + 1);
+
     return (
-        <div className="font-mono text-base sm:text-xl w-96 
-        bg-slate-700 px-2 py-4 rounded-lg  border-t-[20px] border-4 border-slate-200 
+        <div className="font-mono text-sm sm:text-base  w-80 sm:w-[28rem] 
+        bg-slate-700 px-2 py-2 rounded-lg  border-t-[20px] border-4 border-slate-200 
         shadow-lg shadow-black
-        grid grid-cols-[32px_minmax(360px,_1fr)_100px] gap-0 relative">
+        grid grid-cols-[32px_minmax(300px,_1fr)] gap-0 relative">
             <div className="absolute flex gap-1 -top-4">
                 <div className="w-3 h-3 bg-red-500 rounded-full"></div>
                 <div className="w-3 h-3 bg-yellow-500 rounded-full"></div>
                 <div className="w-3 h-3 bg-green-500 rounded-full"></div>
             </div>
             <div className="flex flex-col w-4 text-sky-400">
-                <p>
-                    1.
-                </p>
-                <p>
-                    2.
-                </p>
-                <p>
-                    3.
-                </p>
-                <p>
-                    4.
-                </p>
-                <p>
-                    5.
-                </p>
-                <p>
-                    6.
-                </p>
-
+                {rangeArray.map((v) => (
+                    <p>{v}.</p>
+                ))}
             </div>
             <div>
-                <p className="text-fuchsia-400">&lt;div&gt; </p>
-                <div className="text-lime-300 pl-6">
-                    <Typewriter
-                        options={{
-                            strings: `
+                <HTMLelement element='html' lang='pl'>
+                    <HTMLelement element='head' >
+                        <HTMLelement element='title' >
+                            Twoja wymarzona strona
+                        </HTMLelement>
+                    </HTMLelement>
+                    <HTMLelement element='body' >
+                        <HTMLelement element='div' classes='container'>
+                            <Typewriter
+                                options={{
+                                    strings: `
                      Hej!, mam na imię Jakub <br>
                     chętnie wykonam dla Ciebie <br>
                     piękną stronę internetową!`
-                            ,
-                            autoStart: true,
-                            delay: 40,
+                                    ,
+                                    autoStart: true,
+                                    delay: 40,
 
-                        }}
-                    />
-                </div>
-                <p className="text-fuchsia-400">&lt;/div&gt; </p>
+                                }}
+                            />
+                        </HTMLelement>
+                    </HTMLelement>
+
+                </HTMLelement>
+
+
+
+
             </div>
-        </div>
+        </div >
     )
 }
+
+interface HTMLelementProps {
+    element: string;
+    classes?: string;
+    lang?: string;
+    children: React.ReactNode
+}
+
+const HTMLelement: React.FC<HTMLelementProps> = ({ element, classes, lang, children }) => {
+    return (
+        <div>
+            <p className="text-fuchsia-400">
+                &lt;{element}{classes ? <b className="text-teal-400"> class
+                    <b className="text-sky-400">=</b>
+                    <b className='text-red-400'>"{classes}"</b></b> : null}
+                {lang ? <b className="text-teal-400"> lang
+                    <b className="text-sky-400">=</b>
+                    <b className='text-red-400'>"{lang}"</b></b> : null}&gt;
+            </p>
+            <div className='pl-4 text-lime-300'> {children}</div>
+            <p className="text-fuchsia-400">&lt;/{element}&gt;</p>
+        </div>
+    );
+};
